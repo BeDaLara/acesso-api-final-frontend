f3c6836d82acfac2535d80b3c56a27e008c91650
BeDaLara <bernardosilvacorlaite2406@gmail.com>
2025-03-17 20:19:21 -0300

    listagem de usuario usando api + loading

--- src/router.js
+++ src/router.js
@@ -1,16 +1,39 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Login from '@/components/Login.vue'
-import Home from '@/components/Home.vue'
-
+import Login from '@/components/Login.vue';
+import Home from '@/components/Home.vue';
+import Usuarios from '@/components/Usuarios.vue';
 
 
 Vue.use(Router);
 
-export default new Router({
+const router =  new Router({
     mode: "history",
     routes: [
         { path: "/login", component: Login },
-        { path: "/home", component: Home }
+        { 
+            path: "/home", 
+            component: Home,
+            children: [
+                {path: "usuarios", component: Usuarios},
+            ],
+            meta: { requiresAuth: true}
+        },
     ]
-})
+});
+
+//Intercepta a navegação visando validar regras
+router.beforeEach((to, from, next) => {
+    const usuarioLogado = localStorage.getItem('dados-usuario-logado');
+
+    //Valida se a rota solicitada estar logado e se possui dados de login
+    if(to.matched.some(record => record.meta.requiresAuth) && !usuarioLogado)
+    {
+        // Se a rota exige autenticação e não há usuário logado, redireciona para login
+        next('/login');
+    }
+    // segue a vida
+    next();
+});
+
+export default router;
